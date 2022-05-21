4e797912231b08b45911bc628fd4fd098b3fa3ce
Mateus <mateus.pardinho@gmail.com>
2022-05-20 21:11:15 -0300

    Commit inicial do projeto
    Estrutura básica da API

---
+++ src/main/java/io/github/imateus/sorterapi/SorterApiApplication.java
@@ -0,0 +1,13 @@
+package io.github.imateus.sorterapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SorterApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SorterApiApplication.class, args);
+	}
+
+}
